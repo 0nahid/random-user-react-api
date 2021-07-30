a5ae4a1f224954932f805f57eed4d4a8fa466c47
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-07-31 03:08:32 +0600

    fixed all

--- src/App.js
+++ src/App.js
@@ -15,19 +15,25 @@ function App() {
 function Data() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("https://randomuser.me/api/")
+    fetch("https://randomuser.me/api/?results=5000")
       .then((res) => res.json())
       .then((data) => {
-        setData(data.results[0]);
+        setData(data.results);
       });
   }, []);
   if (data.length < 1) {
     return <h2>Loading </h2>;
   }
   return (
-    <div>
-      <h2>{data.name.first}</h2>
-      <h2>{data.name.last}</h2>
+    <div className="userContainer">
+      <h1>Dynamic Users : {data.length} </h1>
+      {data.map(person => {
+        const { name } = person
+        return <div className="userStyle">
+          <h2>{name.first}</h2>
+          <h2>{name.last}</h2>
+        </div>
+      })}
     </div>
   );
 }
