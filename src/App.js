79a6823b5bb5a70ac82ecaff5cdd72b14d138595
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-07-31 02:24:33 +0600

    error on app.js/ 08 & 26 no line

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Data></Data>
+        <Users></Users>
       </header>
     </div>
   );
 }
 
+function Data() {
+  const [data, setData] = useState([]);
+  useEffect(() =>
+    fetch('https://randomuser.me/api/')
+      .then((response) => response.json())
+      .then(data => setData(data))
+  )
+  return (
+    <div>
+      {data.map(data =>
+        <div>
+          <h1>{data.results[0].name.first}</h1>
+          <h1>{data.results.name.first}</h1>
+        </div>
+      )}
+    </div>
+  )
+}
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() =>
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then(data => setUsers(data))
+  )
+  return (
+    <div className="userContainer">
+      <h1>Dynamic Users : {users.length} </h1>
+      {users.map(user  =>
+        <div className="userStyle">
+          <h1>Name : {user.name} </h1>
+          <h5>Mail : {user.email} </h5>
+        </div>
+      )}
+    </div>
+  )
+}
 export default App;
